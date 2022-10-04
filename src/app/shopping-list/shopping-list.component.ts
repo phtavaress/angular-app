5f322e7f7f162073b4fc236558e44442901ea794
Pedro Henrique <pedrohenrique@MacBook-Air-de-Pedro.local>
2022-10-04 11:29:05 -0300

    Shopping list component edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,8 +17,4 @@ export class ShoppingListComponent implements OnInit {
     this.ingredients = this.shoppingListService.getIngredients();
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-  }
-
 }
