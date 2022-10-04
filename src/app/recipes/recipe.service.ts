f9c1a8536738b6cb4b6f3bd23582dac899323b6f
Pedro Henrique <pedrohenrique@MacBook-Air-de-Pedro.local>
2022-10-04 11:14:48 -0300

    Using a service for Cross-Component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -14,6 +15,8 @@ export class RecipeService {
             'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540')
         
     ];
+
+    recipeSelected = new EventEmitter<Recipe>();
 
     getRecipes() {
         return this.recipes.slice();
