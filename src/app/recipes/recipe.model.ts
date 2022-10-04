bcd6645f292da6dc322c919f24aa01a33eb30e58
Pedro Henrique <pedrohenrique@MacBook-Air-de-Pedro.local>
2022-10-04 11:48:00 -0300

    Adding ingredients to recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
 
     name: string;
     description: string;
     imagePath: string;
+    ingredients: Ingredient[];
 
-    constructor(name: string, description: string, imagePath: string) {
+    constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 
 }
